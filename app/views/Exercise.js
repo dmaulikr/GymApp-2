b655d676c1e8c83805ffb792eeac0438385d2446
Daniel <danielostapenko@gmail.com>
2016-08-24 19:04:37 +0200

    Add Exercise view

---
+++ app/views/Exercise.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class Exercise extends Component {
+  static propTypes = {
+
+  };
+  static defaultProps = {
+
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
